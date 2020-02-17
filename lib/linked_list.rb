3afb004fb72b4f9fdaed4b6ae8537c7069693dc8
KKennedyCodes <holmstedtk@gmail.com>
2020-02-17 09:33:52 -0800

    All tests passing but one

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -16,6 +16,90 @@ class LinkedList
     @head = nil # keep the head private. Not accessible outside this class
   end
   
+  # method to add a new node with the specific data value in the linked list
+  # insert the new node at the beginning of the linked list
+  def add_first(value)
+    @head = Node.new(value, @head)
+    
+    # new_node = Node.new(value)
+    # new_node.next = @head
+    # @head = new_node
+    
+  end
+  
+  # Additional Exercises 
+  # returns the value in the first node
+  # returns nil if the list is empty
+  def get_first
+    if @head.nil?
+      return nil
+    else
+      return @head.data
+    end
+  end
+  
+  # method that returns the length of the singly linked list
+  def length
+    counter = 0
+    current = @head
+    
+    until current.nil?
+      counter += 1
+      current = current.next  
+    end
+    
+    return counter
+  end
+  
+  # method that inserts a given value as a new last node in the linked list
+  def add_last(value)
+    new_node = Node.new(value, nil)
+    current = @head
+    
+    if current.nil?
+      @head = new_node
+    else
+      while current != nil
+        current = current.next  
+      end
+    end
+    current = new_node
+  end
+  
+  # method that returns the value of the last node in the linked list
+  # returns nil if the linked list is empty
+  def get_last
+    if @head.nil?
+      return nil
+    else
+      current = @head
+      
+      until current.next.nil?
+        current = current.next  
+      end
+      
+      return current.data
+    end
+  end
+  
+  # method that returns the value at a given index in the linked list
+  # index count starts at 0
+  # returns nil if there are fewer nodes in the linked list than the index value
+  def get_at_index(index)
+    current = @head
+    
+    index.times do |i|
+      if current.nil?
+        return nil
+      end
+      
+      current = current.next  
+    end
+    return current.data
+  end
+  
+  ##########################################
+  ##########################################
   
   #test with empty list, less than 5 notes, exactly 5 notes
   def del_five
@@ -83,18 +167,6 @@ class LinkedList
     return current.data
   end
   
-  
-  # method to add a new node with the specific data value in the linked list
-  # insert the new node at the beginning of the linked list
-  def add_first(value)
-    @head = Node.new(value, @head)
-    
-    # new_node = Node.new(value)
-    # new_node.next = @head
-    # @head = new_node
-    
-  end
-  
   # method to find if the linked list contains a node with specified value
   # returns true if found, false otherwise
   def search(value)
@@ -128,19 +200,6 @@ class LinkedList
     raise NotImplementedError
   end
   
-  
-  # method that returns the length of the singly linked list
-  def length
-    raise NotImplementedError
-  end
-  
-  # method that returns the value at a given index in the linked list
-  # index count starts at 0
-  # returns nil if there are fewer nodes in the linked list than the index value
-  def get_at_index(index)
-    raise NotImplementedError
-  end
-  
   # method to print all the values in the linked list
   def visit
     raise NotImplementedError
@@ -156,7 +215,6 @@ class LinkedList
   def reverse
     raise NotImplementedError
   end
-  
   
   ## Advanced Exercises
   # returns the value at the middle element in the singly linked list
@@ -174,25 +232,6 @@ class LinkedList
   # linked list links to a node already visited.
   # returns true if a cycle is found, false otherwise.
   def has_cycle
-    raise NotImplementedError
-  end
-  
-  
-  # Additional Exercises 
-  # returns the value in the first node
-  # returns nil if the list is empty
-  def get_first
-    raise NotImplementedError
-  end
-  
-  # method that inserts a given value as a new last node in the linked list
-  def add_last(value)
-    raise NotImplementedError
-  end
-  
-  # method that returns the value of the last node in the linked list
-  # returns nil if the linked list is empty
-  def get_last
     raise NotImplementedError
   end
   
