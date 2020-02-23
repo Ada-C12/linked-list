bbfa3eb85b941245b4318b6cae980e6338f4cdc1
Erika Dawn <erika@Erikas-MacBook-Pro.local>
2020-02-22 22:37:21 -0800

    Pushing from work comp

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -19,56 +19,131 @@ class LinkedList
     # method to add a new node with the specific data value in the linked list
     # insert the new node at the beginning of the linked list
     def add_first(value)
-      raise NotImplementedError
+      @head = Node.new(value, @head)
     end
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
     def search(value)
-      raise NotImplementedError
+        if @head.nil?
+            return nil
+        end
+
+        until @head.nil?
+            if @head.data == value
+                return true
+            else
+                @head = @head.next
+            end
+        end
+        return false
     end
+
 
     # method to return the max value in the linked list
     # returns the data value and not the node
     def find_max
-      raise NotImplementedError
+  
+        return nil if @head.nil?
+        current = @head
+        max = current
+
+        until current.nil?
+            if max.data < current.data
+                max = current
+                current = current.next
+            end
+        end
+        return max.data
     end
 
     # method to return the min value in the linked list
     # returns the data value and not the node
     def find_min
-      raise NotImplementedError
+
+        return nil if @head.nil?
+
+        current = @head
+        min = current
+
+        until current.nil?
+            if min.data > current.data
+                min.data = current.data
+                current = current.next
+            end
+        end
+        return min.data
     end
 
 
     # method that returns the length of the singly linked list
     def length
-      raise NotImplementedError
+      if @head.nil? 
+        return 0
+      end
+      
+      current = @head
+      length = 0
+
+      until current.nil?
+        length += 1
+        current = current.next
+      end
+      
+      return length
+
     end
 
     # method that returns the value at a given index in the linked list
     # index count starts at 0
     # returns nil if there are fewer nodes in the linked list than the index value
-    def get_at_index(index)
-      raise NotImplementedError
-    end
+ 
 
     # method to print all the values in the linked list
     def visit
-      raise NotImplementedError
+        return nil if @head.nil?
+        current = @head
+
+        until current.nil?
+            print current.value
+            current = current.next
+        end
+        
     end
 
     # method to delete the first node found with specified value
     def delete(value)
-      raise NotImplementedError
+
+        return nil if @head.nil?
+        current = @head
+        previous = nil
+
+        until current.nil?
+            if current.data == value
+                previous.next = current.next
+                exit
+            else
+                previous = current
+                current = current.next
+            end
+        end  
+
     end
 
     # method to reverse the singly linked list
     # note: the nodes should be moved and not just the values in the nodes
     def reverse
-      raise NotImplementedError
+        current = @head
+        previous = nil
+        until current.nil?
+          temp = current.next
+          current.next = previous
+          previous = current
+          current = temp
+        end
+    
+        @head = previous
     end
-
 
     ## Advanced Exercises
     # returns the value at the middle element in the singly linked list
